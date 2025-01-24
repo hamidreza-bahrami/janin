7f3f6960229494b0879d41f161523c4ae32d371c
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2025-01-24 11:08:27 -0800

    Update janin.py

--- janin.py
+++ janin.py
@@ -3,10 +3,13 @@ import pandas as pd
 import numpy as np
 import pickle 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import time
 
+st.set_page_config(page_title='تشخیص ناهنجاری جنین - RoboAi', layout='centered', page_icon='👶🏻')
+
 def load_model():
-    with open('model.pkl', 'rb') as file:
+    with open('saved.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
 
@@ -16,48 +19,85 @@ model = data['model']
 x = data['x']
 
 def show_page():
-    st.write("<h1 style='text-align: center; color: blue;'>مدل تشخیص سلامت جنین</h1>", unsafe_allow_html=True)
-    st.write("<h3 style='text-align: center; color: gray;'>برای نتیجه دقیق مشخصات توسط پزشک متخصص وارد شده و ارزیابی گردد</h3>", unsafe_allow_html=True)
-    st.write("<h4 style='text-align: center; color: gray;'>Robo-Ai.ir طراحی شده توسط</h4>", unsafe_allow_html=True)
+    st.write("<h3 style='text-align: center; color: blue;'>تشخیص ناهنجاری جنین 🧬</h3>", unsafe_allow_html=True)
+    st.write("<h6 style='text-align: center; color: black;'>Robo-Ai.ir طراحی شده و توسعه</h6>", unsafe_allow_html=True)
     st.link_button("Robo-Ai بازگشت به", "https://robo-ai.ir")
+    container = st.container(border=True)
+    container.write("<h6 style='text-align: right; color: gray;'>تشخیص ناهنجاری جنین با هوش مصنوعی 🔬</h6>", unsafe_allow_html=True)
+    st.write('')
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal Accelerations / 15 s</h5>", unsafe_allow_html=True)
+    with st.sidebar:
+        st.write("<h5 style='text-align: center; color: blcak;'>تشخیص ناهنجاری جنین با یادگیری ماشین</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: center; color: gray;'>ساخته شده برای تشخیص ناهنجاری جنین بر اساس آزمایش</h5>", unsafe_allow_html=True)
+        st.divider()
+        st.write("<h5 style='text-align: center; color: black;'>طراحی و توسعه</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: center; color: gray;'>حمیدرضا بهرامی</h5>", unsafe_allow_html=True)
+
     accelerations = st.slider('میزان افزایش ضربان قلب جنین در هر 15 ثانیه', 0.00, 0.05, 0.01)
+    st.divider()
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal Movement</h5>", unsafe_allow_html=True)
     fetal_movement = st.slider('میزان رشد جنین که توسط مادر حس می گردد', 0.00, 0.50000, 0.01)
+    st.divider()
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal Prolonged Peceleration</h5>", unsafe_allow_html=True)
     prolongued_decelerations = st.slider('ضربان قلب غیرطبیعی جنین از هر 2 تا 10 دقیقه', 0.000, 0.05, 0.01)
+    st.divider()
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal Abnormal STV</h5>", unsafe_allow_html=True)
     abnormal_short_term_variability = st.slider('تغییر پذیری کوتاه مدت و غیرطبیعی ساقه مغز', 12.00, 90.00, 15.00)
+    st.divider()
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal Abnormal STV Time Percentage</h5>", unsafe_allow_html=True)
     percentage_of_time_with_abnormal_long_term_variability = st.slider('درصد زمانی تغییر پذیری کوتاه مدت ضربان قلب جنین با فرکانس 3 تا 10 سیکل/دقیقه', 0.00, 91.00, 1.00)
+    st.divider()
 
-    st.write("<h5 style='text-align: center; color: gray;'>Fetal LTV Mean Value</h5>", unsafe_allow_html=True)
     mean_value_of_long_term_variability = st.slider('میانگین تنوع بلند مدت', 0.00, 51.00, 5.00)
+    st.divider()
 
-    button = st.button('معاینه و تشخیص')
+    button = st.button('ارزیابی')
     if button:
-        x = np.array([[accelerations, fetal_movement, prolongued_decelerations,
-                       abnormal_short_term_variability, percentage_of_time_with_abnormal_long_term_variability,
-                       mean_value_of_long_term_variability]])
+        with st.chat_message("assistant"):
+                with st.spinner('''درحال ارزیابی ، لطفا صبور باشید'''):
+                    time.sleep(2)
+                    st.success(u'\u2713''ارزیابی انجام شد')
+                    x = np.array([[accelerations, fetal_movement, prolongued_decelerations, abnormal_short_term_variability,
+                                    percentage_of_time_with_abnormal_long_term_variability, mean_value_of_long_term_variability]])
 
         y_prediction = model.predict(x)
         if y_prediction == 1.0:
-            with st.chat_message("assistant"):
-                with st.spinner('''درحال بررسی، لطفا صبور باشید'''):
-                    time.sleep(3)
-                    st.success(u'\u2713''بررسی انجام شد')
-                    st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، جنین سالم است</h4>", unsafe_allow_html=True)
-        
+            text1 = 'بر اساس ارزیابی من ، جنین سالم است'
+            text2 = 'Based on my analysis, Fetus is healthy'
+            def stream_data1():
+                for word in text1.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data1)
+            def stream_data2():
+                for word in text2.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data2)
+            
         else:
-            with st.chat_message("assistant"):
-                with st.spinner('''درحال بررسی، لطفا صبور باشید'''):
-                    time.sleep(3)
-                    st.success(u'\u2713''بررسی انجام شد')
-                    st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، جنین مشکوک به ناهنجاری است</h4>", unsafe_allow_html=True)
-                    st.write("<h5 style='text-align: right; color: gray;'>برای بررسی دقیق تر به پزشک متخصص مراجعه کنید</h5>", unsafe_allow_html=True)
+            text5 = 'بر اساس ارزیابی من ، احتمال وجود ناهنجاری در جنین بالا است'
+            text6 = 'برای ارزیابی بهتر به پزشک متخصص مراجعه کنید'
+            text7 = 'Based on my analysis, the possibility of Fetus being unhealthy is high'
+            text8 = "For more certainty, visit a specialist doctor"
+            def stream_data5():
+                for word in text5.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data5)
+            def stream_data6():
+                for word in text6.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data6)
+            def stream_data7():
+                for word in text7.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data7)
+            def stream_data8():
+                for word in text8.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data8)
 show_page()
